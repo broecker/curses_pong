128cf135f9842206c2ffaf06db43743ae9809d62
Markus Broecker <mbrckr@gmail.com>
2021-03-09 21:07:17 -0800

    Added curses

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+#include <cstdlib>
 #include <iostream>
 
-int main(int argc, const char** argv) {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+#include <curses.h>
+
+void cleanup() { endwin(); }
+
+int main(int argc, const char **argv) {
+  std::cout << "Hello World!" << std::endl;
+
+  initscr();
+  cbreak();
+  noecho();
+  clear();
+
+  refresh();
+  getch();
+
+  atexit(cleanup);
+
+  return 0;
 }
